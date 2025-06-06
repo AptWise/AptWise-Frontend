37a6c61972301f6c5a4ebcf4e846b19c1c97e1db
Auhona Basu <auhonabasu03@gmail.com>
2025-06-06 00:13:01 -0400

    Registration Page Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
+import Register from './pages/Registration';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/Registration" element={<Register />} />
     </Routes>
   );
 };
